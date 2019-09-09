a759de62fa880259d0c68b7018d776d89a46d5de
Jason Bukowski <jbukowski@gmail.com>
2019-09-09 12:33:52 -0400

    Apply prettier formatting

--- example.js
+++ example.js
@@ -1,30 +1,42 @@
-const lightning = require("lnrpc-node-client");
+const lightning = require('lnrpc-node-client')
 
 lightning.setCredentials('34.66.56.153:10009', '/home/jacob/.lnd/admin.macaroon', '/home/jacob/.lnd/tls.cert')
 
 let client = lightning.lightning()
 let unlocker = lightning.unlocker()
-let signer = lightning.signer()
+// let signer = lightning.signer()
 let wallet = lightning.wallet()
-let invoice = lightning.invoice()
+// let invoice = lightning.invoice()
 
-let seed = ""
-let pass = ""
+let seed = ''
+let pass = ''
 
+unlocker.initWallet({ wallet_password: pass, cipher_seed_mnemonic: seed.split(' ') }, (err, res) => {
+  console.log(res)
+  console.log(err)
+})
 
-unlocker.initWallet({wallet_password: pass, cipher_seed_mnemonic: seed.split(' ')}, (err, res) => { console.log(res); console.log(err); });
+unlocker.unlockWallet({ wallet_password: pass }, (err, res) => {
+  console.log(res)
+  console.log(err)
+})
 
+client.getTransactions({ num_confirmations: 793 }, (err, res) => {
+  console.log(res)
+  console.log(err)
+})
 
-unlocker.unlockWallet({wallet_password:pass}, (err, res) => { console.log(res); console.log(err); });
+client.getBlock({ block_height: 793 }, (err, res) => {
+  console.log(res)
+  console.log(err)
+})
 
+wallet.estimateFee({ conf_target: 2 }, (err, res) => {
+  console.log(res)
+  console.log(err)
+})
 
-client.getTransactions({num_confirmations:793}, (err, res) => { console.log(res); console.log(err); });
-
-
-client.getBlock({block_height:793}, (err, res) => { console.log(res); console.log(err); });
-
-
-wallet.estimateFee({conf_target:2}, (err, res) => { console.log(res); console.log(err); });
-
-
-wallet.keyForAddress({addr_in:'tb1qsqmnn66s0ldj7mntam90hgk46uh9q8cnmvqzr3'}, (err, res) => { console.log(res); console.log(err); });
+wallet.keyForAddress({ addr_in: 'tb1qsqmnn66s0ldj7mntam90hgk46uh9q8cnmvqzr3' }, (err, res) => {
+  console.log(res)
+  console.log(err)
+})
--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-const grpc = require('grpc');
-const fs = require("fs");
-const path = require('path');
-const rpcPath = path.join(__dirname, "rpc.proto")
-const signerPath = path.join(__dirname, "signer.proto")
-const walletPath = path.join(__dirname, "walletkit.proto")
-const invoicePath = path.join(__dirname, "invoices.proto")
-
+const grpc = require('grpc')
+const fs = require('fs')
+const path = require('path')
+const rpcPath = path.join(__dirname, 'rpc.proto')
+const signerPath = path.join(__dirname, 'signer.proto')
+const walletPath = path.join(__dirname, 'walletkit.proto')
+const invoicePath = path.join(__dirname, 'invoices.proto')
 
 // Due to updated ECDSA generated tls.cert we need to let gprc know that
 // we need to use that cipher suite otherwise there will be a handhsake
@@ -20,77 +19,77 @@ let wallet
 let invoice
 
 // Convert callbacks to async methods
-exports.promisifyGrpc = function (client) {
-	Object.keys(Object.getPrototypeOf(client)).forEach(function(functionName) {
-		const originalFunction = client[functionName];
-		let newFunc = async (req, mt) => {
-			return new Promise(fill => {
-				originalFunction.bind(client)(req, mt, (err, res) => {
-					fill({ err, value: res });
-				});
-			});
-		};
-		client[functionName + 'Async'] = newFunc;
-	});
+exports.promisifyGrpc = function(client) {
+  Object.keys(Object.getPrototypeOf(client)).forEach(function(functionName) {
+    const originalFunction = client[functionName]
+    let newFunc = async (req, mt) => {
+      return new Promise(fill => {
+        originalFunction.bind(client)(req, mt, (err, res) => {
+          fill({ err, value: res })
+        })
+      })
+    }
+    client[functionName + 'Async'] = newFunc
+  })
 }
 
 // use setCredentials to initialize authenticated grpc connection
-exports.setCredentials = function (socketPath, macaroonPath, tlsCertPath) {
-	var m = fs.readFileSync(macaroonPath);
-	var macaroon = m.toString('hex');
+exports.setCredentials = function(socketPath, macaroonPath, tlsCertPath) {
+  var m = fs.readFileSync(macaroonPath)
+  var macaroon = m.toString('hex')
 
-	var metadata = new grpc.Metadata()
-	metadata.add('macaroon', macaroon)
-	var macaroonCreds = grpc.credentials.createFromMetadataGenerator((_args, callback) => {
-	  callback(null, metadata);
-	});
+  var metadata = new grpc.Metadata()
+  metadata.add('macaroon', macaroon)
+  var macaroonCreds = grpc.credentials.createFromMetadataGenerator((_args, callback) => {
+    callback(null, metadata)
+  })
 
-	var lndCert = fs.readFileSync(tlsCertPath);
-	var sslCreds = grpc.credentials.createSsl(lndCert);
+  var lndCert = fs.readFileSync(tlsCertPath)
+  var sslCreds = grpc.credentials.createSsl(lndCert)
 
-	const lnrpcDescriptor = grpc.load(rpcPath);
-	const signDescriptor = grpc.load(signerPath);
-	const walletDescriptor = grpc.load(walletPath);
-	const invoiceDescriptor = grpc.load(invoicePath);
-	lndHost = socketPath;
-	credentials = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds);
-	lnrpc = lnrpcDescriptor.lnrpc;
-	signer = signDescriptor.signrpc;
-	wallet = walletDescriptor.walletrpc;
-	invoice = invoiceDescriptor.invoicesrpc;
+  const lnrpcDescriptor = grpc.load(rpcPath)
+  const signDescriptor = grpc.load(signerPath)
+  const walletDescriptor = grpc.load(walletPath)
+  const invoiceDescriptor = grpc.load(invoicePath)
+  lndHost = socketPath
+  credentials = grpc.credentials.combineChannelCredentials(sslCreds, macaroonCreds)
+  lnrpc = lnrpcDescriptor.lnrpc
+  signer = signDescriptor.signrpc
+  wallet = walletDescriptor.walletrpc
+  invoice = invoiceDescriptor.invoicesrpc
 }
 
 // use setTls to initialize unauthenticated grpc connection
-exports.setTls = function (socketPath, tlsCertPath) {
-	var lndCert = fs.readFileSync(tlsCertPath);
+exports.setTls = (socketPath, tlsCertPath) => {
+  var lndCert = fs.readFileSync(tlsCertPath)
 
-	const lnrpcDescriptor = grpc.load(rpcPath);
-	lndHost = socketPath;
-	credentials = grpc.credentials.createSsl(lndCert);
-	lnrpc = lnrpcDescriptor.lnrpc;
+  const lnrpcDescriptor = grpc.load(rpcPath)
+  lndHost = socketPath
+  credentials = grpc.credentials.createSsl(lndCert)
+  lnrpc = lnrpcDescriptor.lnrpc
 }
 
-exports.lightning = function (){
-	var lightning = new lnrpc.Lightning(lndHost, credentials);
-	return lightning
+exports.lightning = () => {
+  var lightning = new lnrpc.Lightning(lndHost, credentials)
+  return lightning
 }
 
-exports.unlocker = function (){
-	var unlocker = new lnrpc.WalletUnlocker(lndHost, credentials);
-	return unlocker
+exports.unlocker = () => {
+  var unlocker = new lnrpc.WalletUnlocker(lndHost, credentials)
+  return unlocker
 }
 
-exports.signer = function (){
-	var signrpc = new signer.Signer(lndHost, credentials);
-	return signrpc
+exports.signer = () => {
+  var signrpc = new signer.Signer(lndHost, credentials)
+  return signrpc
 }
 
-exports.wallet = function (){
-	var walletrpc = new wallet.WalletKit(lndHost, credentials);
-	return walletrpc
+exports.wallet = () => {
+  var walletrpc = new wallet.WalletKit(lndHost, credentials)
+  return walletrpc
 }
 
-exports.invoice = function (){
-	var invoicerpc = new invoice.Invoices(lndHost, credentials);
-	return invoicerpc
+exports.invoice = () => {
+  var invoicerpc = new invoice.Invoices(lndHost, credentials)
+  return invoicerpc
 }
